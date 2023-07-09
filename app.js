ea8147a6a7a71a88cb47053d8afe468bf1d7b968
Ethan .B <Aussie_Okay@protonmail.com>
2023-07-09 12:53:43 +0930

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+var twitter_url = "https://twitter.com/_notaussie" 
+var discord_url = "https://discordapp.com/users/1084139494934065184" 
+var guilded_url = "https://guilded.gg/u/notaussie" 
+var github_url = "https://github.com/notaussie" 
+
+// Clear the console
+clear(100)
+
+// Twitter
+function twitter() {
+    console.log("Opened: Twitter")
+    window.open(twitter_url, "_blank");
+}
+
+// Discord
+function discord() {
+    console.log("Opened: Discord")
+    window.open(discord_url, "_blank");
+}
+
+// Guilded
+function guilded() {
+    console.log("Opened: Guilded")
+    window.open(guilded_url, "_blank");
+}
+
+// GitHub
+function github() {
+    console.log("Opened: GitHub")
+    window.open(github_url, "_blank");
+}
